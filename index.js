311dcbd608228acb0a5106efe4760aa9df1ba3a7
banobepascal <banobepascal@gmail.com>
2019-06-19 12:50:57 +0300

    changes on POST

--- index.js
+++ index.js
@@ -19,16 +19,13 @@ app.get('/api/courses/', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
-    const schema = {
-    name: Joi.string().min(3).required()
-   };
-
-   const result = Joi.validate(req.body, schema);
-    if(result.error) {
-        res.status(400).send(result.error.details[0].message);
+    const { error } = validateCourse(req.body);
+    if (error){
+        res.status(400).send(error.details[0].message);
         return;
-    }
+    };
 
+    
     const course = {
         id: courses.length + 1,
         name: req.body.name
