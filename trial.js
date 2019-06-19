16eda6e92bf1ef9f934c39375ccf548ba3562e6c
banobepascal <banobepascal@gmail.com>
2019-06-19 14:50:18 +0300

    testing

--- trial.js
+++ trial.js
@@ -2,8 +2,18 @@
 const express = require('express');
 const app = new express();
 
+const people = [
+    { id: 1, name: 'pascal'},
+    { id: 2, name: 'bonny'},
+    { id: 3, name: 'francis'},
+]
+
 app.get('/', (req, res) => {
     res.send('Hello Pascal');
+});
+
+app.get('/hey/people', (req, res) => {
+    res.send(people);
 });
 
 const port = process.env.PORT || 3000;
