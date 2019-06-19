f0e35c79eee0624605d3c04faf5093c2f3c2f5db
banobepascal <banobepascal@gmail.com>
2019-06-19 19:30:08 +0300

    third party middleware

--- trial.js
+++ trial.js
@@ -6,12 +6,19 @@ const auth = require('./auth');
 const express = require('express');
 const app = new express();
 
+// console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+// console.log(`app: ${app.get('env')}`);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(helmet());
-app.use(morgan('tiny'));
 
+
+if (app.get('env') === 'development') {
+    app.use(morgan('tiny'));
+    console.log('Morgan enabled ...');
+}
 
 // creating custom middleware
 app.use(logger);
